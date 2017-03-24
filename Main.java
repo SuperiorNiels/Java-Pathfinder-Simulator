26db611a796a26a070e762b5e81deaa1cfa2dcb1
Niels Balemans <balemans.niels@gmail.com>
2017-03-24 21:45:54 +0100

    Changed Look and Feel

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
 
 	public static void main(String[] args) {
+		try {
+	        UIManager.setLookAndFeel(
+		            UIManager.getSystemLookAndFeelClassName());
+		} 
+	    catch (UnsupportedLookAndFeelException e) {}
+	    catch (ClassNotFoundException e) {}
+	    catch (InstantiationException e) {}
+	    catch (IllegalAccessException e) {}
+		
 		GUI window = new GUI("Pathfinder Simulator");
 		window.setBounds(30,30,1000,800);
 		window.setVisible(true);
