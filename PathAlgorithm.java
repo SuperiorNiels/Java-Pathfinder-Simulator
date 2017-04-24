77804b6e4989c6bf6e51ce5ae5ead5d88a00542e
Dieter97 <dieter.balemans@hotmail.com>
2017-04-24 22:32:55 +0200

    add pathalgorithm

--- PathAlgorithm.java
+++ PathAlgorithm.java
@@ -2,17 +2,46 @@
 public class PathAlgorithm {
 	Settings settings;
 	Algorithm a;
-	public PathAlgorithm(Settings s) {
-		this.settings = s;
-		switch(settings.getAlgorithm()) {
-		case "Breadth-first search": a = new BFS(settings.getMaze()); break;
-		case "Dijkstra" : a = new Dijkstra(settings.getMaze()); break;
+	public PathAlgorithm(Settings settings) {
+		this.settings = settings;
+		switch(settings.getAlgorithm()){
+			case "Breadth-first search": 
+				a = new BFS(settings);
+				break;
+			case "Dijkstra": 
+				a = new Dijkstra(settings);
+				break;
+			case "Depth-first search": 
+				a = new DFS(settings);
+				break;
+			default:
+				a = new BFS(settings);
+				break;
 		}
 	}
 	
-	public int[][] solve() {
-		int [][] res = a.solve(settings.getDiagonal());
-		a.dispose();
-		return res;
+	public int[][] getSolution() {
+		return a.getSolution();
 	}
+	
+	public int getIterations() {
+		return a.getIterations();
+	}
+	
+	public void intitialze() {
+		a.initialize();
+	}
+	
+	public int[][] getNextStep() {
+		return a.getNextStep();
+	}
+	
+	public Boolean solved() {
+		return a.solved();
+	}
+	
+	public Boolean running() {
+		return a.running();
+	}
+	
 }
